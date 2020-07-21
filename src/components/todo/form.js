8c42e8c6f26087e61ffb064b0b92dedf31ec4343
d-d-wolfe <dave.wolfe623@gmail.com>
2020-07-20 17:38:54 -0700

    intitial changes in the form.js

---
+++ src/components/todo/form.js
@@ -0,0 +1,69 @@
+import React, { useState } from 'react';
+
+const TodoForm = (props) => {
+  const [item, setItem] = useState({});
+
+  // const [state, setState] = useState({ item: props.item });
+
+  console.log('this is the item:', item);
+
+  const handleInputChange = (e) => {
+    setItem({ ...item, [e.target.name]: e.target.value } );
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    e.target.reset();
+    props.handleSubmit(item);
+    setItem({});
+  };
+
+  // render() {
+  return (
+    <>
+      <h3>Add Item</h3>
+      <form onSubmit={handleSubmit}>
+        <label>
+          <span>To Do Item</span>
+          <input
+            name="text"
+            placeholder="Add To Do List Item"
+            onChange={handleInputChange}
+          />
+        </label>
+        <label>
+          <span>Difficulty Rating</span>
+          <input defaultValue="1" type="range" min="1" max="5" name="difficulty" onChange={handleInputChange} />
+        </label>
+        <label>
+          <span>Assigned To</span>
+          <input type="text" name="assignee" placeholder="Assigned To" onChange={handleInputChange} />
+        </label>
+        <button>Add Item</button>
+      </form>
+    </>
+  );
+};
+// }
+
+// class TodoForm extends React.Component {
+
+//   constructor(props) {
+//     super(props);
+//     this.state = { item: {} };
+//   }
+// handleInputChange = e => {
+//   this.setState({ item: {...this.state.item, [e.target.name]: e.target.value } });
+// };
+
+// handleSubmit = (e) => {
+//   e.preventDefault();
+//   e.target.reset();
+//   this.props.handleSubmit(this.state.item);
+//   const item = {};
+//   this.setState({item});
+// };
+
+// }
+
+export default TodoForm;
